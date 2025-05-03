e9c6eb73962765c62fed331641d9755b4a837a5e
Vitaly Markov <vymarkov@gmail.com>
2025-05-03 16:59:05 +0100

    fix: update flake.nix to properly handle nix-darwin installation

--- flake.nix
+++ flake.nix
@@ -18,21 +18,6 @@
         leapp = let
           # Determine the correct binary based on platform
           platformInfo = {
-            "x86_64-linux" = {
-              url = "https://github.com/Noovolari/leapp/releases/latest/download/Leapp-x86_64.AppImage";
-              sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-              filename = "Leapp.AppImage";
-            };
-            "aarch64-linux" = {
-              url = "https://github.com/Noovolari/leapp/releases/latest/download/Leapp-aarch64.AppImage";
-              sha256 = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
-              filename = "Leapp.AppImage";
-            };
-            "x86_64-darwin" = {
-              url = "https://github.com/Noovolari/leapp/releases/latest/download/Leapp.dmg";
-              sha256 = "sha256-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=";
-              filename = "Leapp.dmg";
-            };
             "aarch64-darwin" = {
               sha256 = "sha256-c/+gMN1ZKT92O9kDj192B0KbxQyCbevk5i1TnZyM0iw=";
               url = "https://asset.noovolari.com/latest/Leapp-0.26.1-mac-arm64.zip";
@@ -43,29 +28,41 @@
         in pkgs.stdenv.mkDerivation {
           pname = "leapp";
           appName = "Leapp.app";
-          version = "latest";
+          version = "0.26.1";
 
           src = pkgs.fetchurl {
             url = platformInfo.url;
             sha256 = platformInfo.sha256;
           };
 
-          # nativeBuildInputs = pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.hfsprogs pkgs.unzip;
-
-          nativeBuildInputs = [ pkgs.unzip ];
+          nativeBuildInputs = with pkgs; [
+            unzip
+            hfsprogs
+            dmg2img
+          ];
 
           unpackPhase = ''
-            unzip $src -d extracted
-            export sourceRoot=$PWD/extracted
+            unzip $src
+            dmg2img Leapp-0.26.1-arm64.dmg Leapp.img
+            mkdir -p mnt
+            hdiutil attach -imagekey diskimage-class=CRawDiskImage -mountpoint mnt Leapp.img
           '';
 
           installPhase = ''
             runHook preInstall
-            mkdir -p $out/{Applications/${appName},bin}
-            cp -R $sourceRoot/release/Leapp-0.26.1-arm64.dmg $out/Applications/${appName}
+            mkdir -p $out/Applications
+            cp -R mnt/${appName} $out/Applications/
+            hdiutil detach mnt
             runHook postInstall
           '';
 
+          meta = with pkgs.lib; {
+            description = "Leapp - The Cloud Access App";
+            homepage = "https://www.leapp.cloud/";
+            license = licenses.unfree;
+            platforms = [ "aarch64-darwin" ];
+            maintainers = [ ];
+          };
         };
 
       in {
